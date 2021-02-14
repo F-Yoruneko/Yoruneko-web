aa8a9e503bc3ceab25a516009c8b3bf3643e6068
Sota Yamaguchi <area029.0709@gmil.com>
2021-02-14 20:38:59 +0900

    react-bootstrap導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { AppProps } from 'next/app';
 
 import '@/styles/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Layout from '@/components/templates/Layout';
 
 const MyApp = (props: AppProps) => {
   const { Component, pageProps } = props;
 
   return (
-    <>
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 };
 
